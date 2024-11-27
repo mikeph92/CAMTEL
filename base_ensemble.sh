b2368121df264c979463fa657b605626993d1c08
mikeph92 <phamhoangphuc92@gmail.com>
2024-11-28 01:00:12 +1100

    update base methods

---
+++ base_ensemble.sh
@@ -0,0 +1,17 @@
+python base_ensemble.py \
+--classification-task 'tumor' \
+--model 'UNI' \
+--crop-size 96 \
+--testset 'pannuke'
+
+python base_ensemble.py \
+--classification-task 'tumor' \
+--model 'UNI' \
+--crop-size 96 \
+--testset 'ocelot'
+
+python base_ensemble.py \
+--classification-task 'tumor' \
+--model 'UNI' \
+--crop-size 96 \
+--testset 'nucls'
---
+++ base_normalization.sh
@@ -0,0 +1,17 @@
+python base_normalization.py \
+--classification-task 'tumor' \
+--model 'UNI' \
+--crop-size 96 \
+--testset 'pannuke'
+
+python base_normalization.py \
+--classification-task 'tumor' \
+--model 'UNI' \
+--crop-size 96 \
+--testset 'ocelot'
+
+python base_normalization.py \
+--classification-task 'tumor' \
+--model 'UNI' \
+--crop-size 96 \
+--testset 'nucls'
