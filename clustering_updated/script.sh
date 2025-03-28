69a0aa100e378c8e7b738a87ec246b26149d6480
mikeph92 <phamhoangphuc92@gmail.com>
2025-03-29 04:26:21 +1100

    try new clustering with Kmeans

--- clustering_updated/script.sh
+++ clustering_updated/script.sh
@@ -1,5 +1,5 @@
-conda create -n rapids-env -c rapidsai -c nvidia -c conda-forge \
-    rapids=23.10 python=3.9 cudatoolkit=11.8
+# conda create -n rapids-env -c rapidsai -c nvidia -c conda-forge \
+#     rapids=23.10 python=3.9 cudatoolkit=11.8
 
 conda activate rapids-env
 
